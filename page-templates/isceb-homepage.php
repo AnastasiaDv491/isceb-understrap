3930a2bccba26b0856850cdc60f8400df080e1e2
AnastasiaDv491 <anadvoryanchikova@gmail.com>
2021-05-22 18:41:48 +0200

    hompepage!

--- page-templates/isceb-homepage.php
+++ page-templates/isceb-homepage.php
@@ -30,51 +30,51 @@ $container = get_theme_mod('understrap_container_type');
 		<section class="homepage-banners">
 			<div class="container-banners">
 				<div class="item">
-					<div class="bannerCardImage" style="background-image: linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)),url(http://localhost/www/wp-content/uploads/2020/09/bantersnaps-5U_28ojjgms-unsplash-scaled.jpg);">
+					<div class="bannerCardImage" style="background-image: linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)),url(http://localhost:8080/wpISCEB2/wp-content/uploads/2021/02/67676-scaled.jpg);">
 					</div>
 					<div class="bannerCardTag">
 						<p class="banerTagText">Education</p>
 					</div>
 					<div class="bannerCardContent">
-							<h3 class="bannerCardTitle">Event 1</h3>
-							<p class="bannerCardDescription">It is a long established fact that</p>
-							<button class="bannerCardButton">More Info</button>
-					</div>						
+						<h3 class="bannerCardTitle">Event 1</h3>
+						<p class="bannerCardDescription">It is a long established fact that</p>
+						<button class="bannerCardButton">More Info</button>
+					</div>
 				</div>
 				<div class="item">
-					<div class="bannerCardImage" style="background-image: linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)),url(http://localhost/www/wp-content/uploads/2020/09/bannerCampusCropKleiner-scaled.jpg);">
+					<div class="bannerCardImage" style="background-image: linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)),url(http://localhost:8080/wpISCEB2/wp-content/uploads/2021/02/67676-scaled.jpg);">
 					</div>
 					<div class="bannerCardTag">
 						<p class="banerTagText">Diversity</p>
 					</div>
 					<div class="bannerCardContent">
-							<h3 class="bannerCardTitle">Event 2</h3>
-							<p class="bannerCardDescription">It is a long established fact that</p>
-							<button class="bannerCardButton">More Info</button>
+						<h3 class="bannerCardTitle">Event 2</h3>
+						<p class="bannerCardDescription">It is a long established fact that</p>
+						<button class="bannerCardButton">More Info</button>
 					</div>
 				</div>
 				<div class="item">
-					<div class="bannerCardImage" style="background-image: linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)),url(http://localhost/www/wp-content/uploads/2020/09/xps-dpbXgTh0Lac-unsplash-1.jpg);">
+					<div class="bannerCardImage" style="background-image: linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)),url(http://localhost:8080/wpISCEB2/wp-content/uploads/2021/02/67676-scaled.jpg);">
 					</div>
 					<div class="bannerCardTag">
 						<p class="banerTagText">Development</p>
 					</div>
 					<div class="bannerCardContent">
-							<h3 class="bannerCardTitle">Event 3</h3>
-							<p class="bannerCardDescription">It is a long established fact that</p>
-							<button class="bannerCardButton">More Info</button>
+						<h3 class="bannerCardTitle">Event 3</h3>
+						<p class="bannerCardDescription">It is a long established fact that</p>
+						<button class="bannerCardButton">More Info</button>
 					</div>
 				</div>
 				<div class="item">
-					<div class="bannerCardImage" style="background-image: linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)),url(http://localhost/www/wp-content/uploads/2020/09/xps-dpbXgTh0Lac-unsplash-1.jpg);">
+					<div class="bannerCardImage" style="background-image: linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)),url(http://localhost:8080/wpISCEB2/wp-content/uploads/2021/02/67676-scaled.jpg);">
 					</div>
 					<div class="bannerCardTag">
 						<p class="banerTagText">Incoming</p>
 					</div>
 					<div class="bannerCardContent">
-							<h3 class="bannerCardTitle">Event 4</h3>
-							<p class="bannerCardDescription">It is a long established fact that</p>
-							<button class="bannerCardButton">More Info</button>
+						<h3 class="bannerCardTitle">Event 4</h3>
+						<p class="bannerCardDescription">It is a long established fact that</p>
+						<button class="bannerCardButton">More Info</button>
 					</div>
 				</div>
 			</div>
@@ -92,6 +92,51 @@ $container = get_theme_mod('understrap_container_type');
 				</div>
 			</div>
 		</section>
+
+
+		<section class="homepage-about">
+			<div class="container-about">
+				<h2>
+					International Student Council of Economics and Business
+				</h2>
+				<p class="paragraph-about">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tristique tincidunt lorem non feugiat. Curabitur lobortis suscipit ligula, rutrum sollicitudin diam porta ut. Sed egestas vitae neque et volutpat. Cras in elit sed mi placerat rutrum a ut ex
+				</p>
+			</div>
+			<div class="flex-container">
+				<div class="item-about">
+					<i class="fa fa-comment-o" aria-hidden="true" stye=""></i>
+					<h2>What?</h2>
+					<p class="paragraph-item-about">"Colour is a dance between your brain and the world. Every colour is a universe of its own, it unfolds differently and it evokes different feelings and emotions within us.”</p>
+				</div>
+				<div class="item-about">
+					<i class="fa fa-comment-o" aria-hidden="true"></i>
+					<h2>Why?</h2>
+
+					<p class="paragraph-item-about">"Colour is a dance between your brain and the world. Every colour is a universe of its own, it unfolds differently and it evokes different feelings and emotions within us.”</p>
+				</div>
+				<div class="item-about">
+					<i class="fa fa-comment-o" aria-hidden="true"></i>
+					<h2>How?</h2>
+
+					<p class="paragraph-item-about">"Colour is a dance between your brain and the world. Every colour is a universe of its own, it unfolds differently and it evokes different feelings and emotions within us.”</p>
+				</div>
+			</div>
+		</section>
+
+		<section class="homepage-meet-reps">
+			<div class="container-meet-reps">
+				<div class="container-image-meet-reps">
+					<img class="image-meet-reps" src="http://localhost:8080/wpISCEB2/wp-content/uploads/2021/02/67676-scaled.jpg">
+					<div class="list-meet-reps">
+						<div class="list-meet-reps-item">Meet</div>
+						<div class="list-meet-reps-item">Your</div>
+						<div class="list-meet-reps-item">Student representatives</div>
+					</div>
+				</div>
+
+			</div>
+		</section> 
 
 
 		<div class="row">
