6da5e190dbc33f47952b7f6b6014981afc7fd46f
Stijn Pinkhof <stijn.pinkhof@skynet.be>
2021-07-31 14:50:31 +0200

    dropdown

--- sidebar-templates/sidebar-isceb-wiki.php
+++ sidebar-templates/sidebar-isceb-wiki.php
@@ -19,7 +19,7 @@ $sidebar_pos = get_theme_mod('understrap_sidebar_position');
     <div class="isceb-nav-header">
 
 
-        <h4><a href="<?php echo (get_site_url().'/wiki')?>" id="isceb-wiki-nav-header-text"><i class="fa fa-home" aria-hidden="true"></i>ISCEB Wiki</a></h4>
+        <h4><a href="<?php echo (get_site_url() . '/wiki') ?>" id="isceb-wiki-nav-header-text"><i class="fa fa-home" aria-hidden="true"></i>ISCEB Wiki</a></h4>
 
     </div>
 
@@ -58,9 +58,9 @@ $sidebar_pos = get_theme_mod('understrap_sidebar_position');
                 $title_of_page = $programs_of_phase[0]->post_title;
                 break;
             case 'course':
-                $phases_of_course = get_field('phases',$post->ID);
-                $programs_of_phase = get_field('program',$phases_of_course[0]->ID);
-                
+                $phases_of_course = get_field('phases', $post->ID);
+                $programs_of_phase = get_field('program', $phases_of_course[0]->ID);
+
                 $wiki_phases = get_posts(array(
                     'post_type' => 'phase',
                     'order' => 'ASC',
@@ -75,11 +75,11 @@ $sidebar_pos = get_theme_mod('understrap_sidebar_position');
                 $title_of_page = $programs_of_phase[0]->post_title;
                 break;
         }
-        
+
 
 
     ?>
-        
+
         <h4 class="isceb-nav-program-name"> <?php echo $title_of_page ?></h4>
         <?php
         if ($wiki_phases) : ?>
@@ -99,11 +99,17 @@ $sidebar_pos = get_theme_mod('understrap_sidebar_position');
                     ));
                 ?>
 
+                    <div>
+                        <i class="fa fa-sort-desc" aria-hidden="true"
+                        id="isceb-wiki-toggle<?php echo $phase->post_title?>"
+                        onclick="isceb_wiki_sidebar_toggle_function('<?php echo ('isceb-wiki-courses-per-phase-' . $phase->post_title) ?>',this)"
+                        ></i>
+                        <a href="<?php echo get_permalink($phase->ID); ?>" class="isceb-wiki-nav-phase" > <?php echo $phase->post_title; ?> </a>
 
-                    <a href="<?php echo get_permalink($phase->ID); ?>" class="isceb-wiki-nav-phase"> <?php echo $phase->post_title; ?> </a>
+                    </div>
                     <hr class="isceb-wiki-nav-phase-line">
 
-                    <ul class="isceb-wiki-courses-per-phase">
+                    <ul class="isceb-wiki-courses-per-phase" id="<?php echo ('isceb-wiki-courses-per-phase-' . $phase->post_title) ?>" style="display: block;">
                         <?php foreach ($wiki_courses  as $course) :
                         ?>
                             <li>
@@ -120,3 +126,19 @@ $sidebar_pos = get_theme_mod('understrap_sidebar_position');
     ?>
     <button class="isceb-wiki-button-not-gb" id="isceb-wiki-nav-upload-btn"> Upload files </button>
 </div>
+
+<script>
+    function isceb_wiki_sidebar_toggle_function(elementId,dropdownId) {
+        console.log(dropdownId);
+        
+        console.log(elementId);
+        var x = document.getElementById(elementId);
+        if (x.style.display === "block") {
+            x.style.display = "none";
+            dropdownId.classList.add("fa-rotate-270");
+        } else {
+            x.style.display = "block";
+            dropdownId.classList.remove("fa-rotate-270");
+        }
+    }
+</script>
