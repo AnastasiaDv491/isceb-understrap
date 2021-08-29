1680e5e1984b89b6233e7491a36acb255b6fc050
Stijn Pinkhof <stijn.pinkhof@skynet.be>
2021-08-29 20:49:34 +0200

    font change

--- header.php
+++ header.php
@@ -20,6 +20,9 @@ $container = get_theme_mod('understrap_container_type');
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
 	<?php wp_head(); ?>
 </head>
 
