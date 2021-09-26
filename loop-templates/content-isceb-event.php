554e303c550bc7891921d13ca4c741ca7989b3c0
AnastasiaDv491 <anadvoryanchikova@gmail.com>
2021-09-26 14:02:04 +0200

    event card styling draft

--- loop-templates/content-isceb-event.php
+++ loop-templates/content-isceb-event.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Single post partial template
  *
@@ -6,13 +7,13 @@
  */
 
 // Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 echo 'hello<br><br>';
 global $event_template_data;
 print_r($event_template_data);
 
-$isceb_wc_event = wc_get_product( $event_template_data['event_post_id']);
-var_dump(wc_get_product( $event_template_data['event_post_id']));
+$isceb_wc_event = wc_get_product($event_template_data['event_post_id']);
+var_dump(wc_get_product($event_template_data['event_post_id']));
 
 ?>
 
@@ -21,12 +22,52 @@ var_dump(wc_get_product( $event_template_data['event_post_id']));
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 	<header class="entry-header">
 
-		<h1 class="entry-title"><?php echo $isceb_wc_event->get_name()?> </h1>
-		<?php echo $isceb_wc_event->get_price()?>
+		<h1 class="entry-title"><?php echo $isceb_wc_event->get_name() ?> </h1>
+		<?php echo $isceb_wc_event->get_price() ?>
 
 
 	</header><!-- .entry-header -->
 
+	<head>
+		<link rel="stylesheet" href="/style.css" />
+	</head>
+
+	<div class="container">
+		<div class="card-media">
+			<!-- media container -->
+			<div class="card-media-object-container">
+				<div class="card-media-object" style="
+          background-image: url(https://s9.postimg.cc/y0sfm95gv/prince_f.jpg);
+
+          opacity: 0.2;
+        "></div>
+				<!-- <span class="card-media-object-tag subtle">Selling Fast</span> -->
+				<div class="card-media-countdown-container">
+					<span class="card-media-object-tag-countdown-number subtle">3</span>
+					<span class="card-media-object-tag-countdown-text subtle">days left to register</span>
+				</div>
+				<span class="card-media-object-tag subtle">Pride Week </span>
+			</div>
+			<!-- body container -->
+			<div class="card-media-body">
+				<div class="card-media-body-top">
+					<span class="card-media-body-supporting-bottom-text subtle description">Join us at the colorful & diverse Pride Week! Contribute to
+						celebrating love across all genders</span>
+					<div class="card-media-body-top-icons u-float-right"></div>
+				</div>
+
+				<div class="card-media-body-middle">
+					<span class="subtle">Mon, APR 09</span>
+					<span class="subtle">7:00 PM</span>
+					<span class="card-media-body-supporting-bottom-text subtle u-float-right">Free &ndash; $30</span>
+				</div>
+				<div class="card-media-body-bottom">
+					<span class="card-media-body-bottom-text">Read more</span>
+					<button class="card-media-body-bottom-button">Sold out</button>
+				</div>
+			</div>
+		</div>
+	</div>
 
 	<div class="entry-content">
 
@@ -35,7 +76,7 @@ var_dump(wc_get_product( $event_template_data['event_post_id']));
 		<?php
 		wp_link_pages(
 			array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
+				'before' => '<div class="page-links">' . __('Pages:', 'understrap'),
 				'after'  => '</div>',
 			)
 		);
