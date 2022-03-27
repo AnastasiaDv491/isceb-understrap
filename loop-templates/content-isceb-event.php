47ca8380d428f376da6779ba903a48c47160ad2b
Stijn Pinkhof <stijn.pinkhof@skynet.be>
2022-03-27 10:43:59 +0200

    old commit

--- loop-templates/content-isceb-event.php
+++ loop-templates/content-isceb-event.php
@@ -9,6 +9,7 @@
 // Exit if accessed directly.
 defined('ABSPATH') || exit;
 
+/* Code should be moved to seperate plugin for events */
 global $event_template_data;
 
 $isceb_wc_event = wc_get_product($event_template_data['event_post_id']);
@@ -34,6 +35,7 @@ $isceb_event_location_without_tags = strip_tags($event_template_data['location_e
 $isceb_event_location_trimmed = strlen($isceb_event_location_without_tags) > 35 ?  substr($isceb_event_location_without_tags, 0, 35) . "..." : $isceb_event_location_without_tags;
 // var_dump($event_template_data['end_event']);
 
+/* TODO: #14 Better logic needed e.g. what if event is multiple days*/
 $isceb_event_current_date = new DateTime('now');
 $isceb_event_start_date = new DateTime($event_template_data['start_event'][0]);
 
@@ -47,14 +49,14 @@ If yes, return
 
 <a href="
 <?php if ($isceb_event_countdown->invert == 0) : ?> 
-	<?php esc_html_e($isceb_wc_event->get_permalink()); ?>" class="isceb-event-card-link" >
+	<?php esc_html_e($isceb_wc_event->get_permalink()); ?>" class="isceb-event-card-link">
 <?php else : ?>
 	#" class="isceb-event-card-link disabled" >
 <?php endif; ?>
 
 
-	<div class="isceb-event-card">
-		<div class="isceb-event-img-container" style="
+<div class="isceb-event-card">
+	<div class="isceb-event-img-container" style="
 			<?php if (has_post_thumbnail($event_template_data['event_post_id'])) : ?>
 			background-image: url(<?php esc_attr_e(get_the_post_thumbnail_url($event_template_data['event_post_id'])) ?>);
 			<?php else : ?>
@@ -62,54 +64,56 @@ If yes, return
 
 			<?php endif; ?>
 			">
-		</div>
-		<div class="isceb-event-message-box">
-			<?php if ($isceb_event_countdown->invert == 0) : ?>
-				<?php if ($isceb_event_countdown->d < 5) : ?>
-					<h6><?php esc_html_e($isceb_event_countdown->d) ?> Days left</h6>
-				<?php elseif ($isceb_event_countdown->d > 5) : ?>
-					<h6> Get your tickets NOW! </h6>
-				<?php endif; ?>
-			<?php else : ?>
-				<h6>Event has passed</h6>
-			<?php endif; ?>
-		</div>
-		<div class="isceb-event-title">
-			<h4><?php esc_html_e($isceb_wc_event->get_name()); ?></h4>
-			<!-- <p><?php esc_html_e($isceb_event_description_trimmed) ?></p> -->
-		</div>
-
-		<div class="isceb-event-card-metadata">
-
-
-			<?php if ($event_time_obj) : ?>
-				<div class="isceb-event-card-date-time">
-					<div class="isceb-event-card-date">
-						<i class="far fa-calendar-alt"></i><?php esc_html_e($event_start_day_text) ?>
-					</div>
-					<div>
-						<i class="far fa-clock"></i><?php esc_html_e($event_start_time_text) ?>
-					</div>
-				</div>
-			<?php endif; ?>
-
-			<?php if ($event_template_data['location_event'] && $event_template_data['location_event'][0] !== '') : ?>
-				<i class="fas fa-map-marker-alt"></i><?php esc_html_e($isceb_event_location_trimmed) ?>
-			<?php endif; ?>
-
-
-			<?php if ($price_event != '') : ?>
-				<div>
-					<i class="fas fa-ticket-alt"></i><?php echo (isceb_get_price_html_zero_free($isceb_wc_event)) ?>
-				</div>
-
-			<?php endif; ?>
-		</div>
-
-		<div class="isceb-event-card-order-button">
-			<p>Order</p>
-			<i class="fa far fa-chevron-right isceb-event-card-arrow"></i>
-		</div>
 	</div>
+	<div class="isceb-event-message-box">
+		<?php if ($isceb_event_countdown->invert == 0) : ?>
+			<?php if ($isceb_event_countdown->d < 1) : ?>
+				<h6>Event is today</h6>
+			<?php elseif ($isceb_event_countdown->d < 5) : ?>
+				<h6><?php esc_html_e($isceb_event_countdown->d) ?> Days left</h6>
+			<?php elseif ($isceb_event_countdown->d > 5) : ?>
+				<h6> Get your tickets NOW! </h6>
+			<?php endif; ?>
+		<?php else : ?>
+			<h6>Event has passed</h6>
+		<?php endif; ?>
+	</div>
+	<div class="isceb-event-title">
+		<h4><?php esc_html_e($isceb_wc_event->get_name()); ?></h4>
+		<!-- <p><?php esc_html_e($isceb_event_description_trimmed) ?></p> -->
+	</div>
+
+	<div class="isceb-event-card-metadata">
+
+
+		<?php if ($event_time_obj) : ?>
+			<div class="isceb-event-card-date-time">
+				<div class="isceb-event-card-date">
+					<i class="far fa-calendar-alt"></i><?php esc_html_e($event_start_day_text) ?>
+				</div>
+				<div>
+					<i class="far fa-clock"></i><?php esc_html_e($event_start_time_text) ?>
+				</div>
+			</div>
+		<?php endif; ?>
+
+		<?php if ($event_template_data['location_event'] && $event_template_data['location_event'][0] !== '') : ?>
+			<i class="fas fa-map-marker-alt"></i><?php esc_html_e($isceb_event_location_trimmed) ?>
+		<?php endif; ?>
+
+
+		<?php if ($price_event != '') : ?>
+			<div>
+				<i class="fas fa-ticket-alt"></i><?php echo (isceb_get_price_html_zero_free($isceb_wc_event)) ?>
+			</div>
+
+		<?php endif; ?>
+	</div>
+
+	<div class="isceb-event-card-order-button">
+		<p>Order</p>
+		<i class="fa far fa-chevron-right isceb-event-card-arrow"></i>
+	</div>
+</div>
 
 </a>
